fb0ac6c7ddd094c813c9016f565f43a9f2a37c11
monicatemburu <saimonicasowmyasri@gmail.com>
2021-12-18 16:21:26 +0530

    Initial commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 
 var path = require('path');
 
-const JWT_SECRET = 'kdoelwoieoewdlasdandakdgdiahdkdsjhdw'
+const JWT_SECRET = 'kdoelwoieoewdlasdhdikhdkwhkewho3wusmasmaksasp;kdmasdl'
 mongoose.connect('mongodb://localhost/myFirstDatabase',{
     useNewUrlParser: true,
     useUnifiedTopology:true,
@@ -56,7 +56,6 @@ app.post('/register', async (req,res) => {
 
 app.post('/login', async(req,res)=>{
     
-    console.log(decoded)
     const {username, password} = req.body
     console.log(username)
     const user = await User.findOne({ username }).lean()
@@ -64,7 +63,8 @@ app.post('/login', async(req,res)=>{
     if(!user){
         return res.json({status:'error', error:'Invalid username/password'})
     }
-
+    const token = jwt.sign({id: user._id, username: user.username}, JWT_SECRET)
+    console.log(token)
     if(await bcrypt.compare(password, user.password)){
         const token = jwt.sign({id: user._id, username: user.username}, JWT_SECRET)
         return res.json({status: 'ok', data: token})
@@ -74,8 +74,13 @@ app.post('/login', async(req,res)=>{
 
 app.post('/changepassword', async(req,res)=>{
     const {token, newpassword: plainTextPassword} = req.body
+    console.log(token)
+    console.log(plainTextPassword)
+    console.log(!plainTextPassword)
+    console.log(typeof plainTextPassword != 'string')
+    console.log(typeof(plainTextPassword))
     if(!plainTextPassword || typeof plainTextPassword != 'string'){
-        return res.json({status: 'ok', error: 'Invalid password'})
+        return res.json({status: 'error', error: 'Invalid password'})
     }
     if(plainTextPassword.length < 5){
         return res.json({status: 'error', error: 'Password is too short, it should be atleast 6 characters'})
@@ -83,11 +88,12 @@ app.post('/changepassword', async(req,res)=>{
     try{
         const user = await jwt.verify(token, JWT_SECRET)
         const _id = user.id
+        console.log(_id)
         const password = await bcrypt.hash(plainTextPassword,10)
         await User.updateOne({ _id },{
             $set: {password}
         })
-        res.json({status:'ok',})
+        res.json({status:'ok', data:req.body.token})
     }
     catch(err){
         if(err){
